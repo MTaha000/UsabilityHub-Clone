5ae3f1b0df6fdaed5b410e35f585a15357a97b6d
Taha <itstaha82@gmail.com>
2024-02-20 23:18:49 +0500

    Added Scroll Page Animation

--- javaScript.js
+++ javaScript.js
@@ -1,12 +1,31 @@
 const menuBtn = document.querySelector(".menubar i")
-const cancel = document.querySelector(".fa-xmark") 
+const cancel = document.querySelector(".fa-xmark")
 const navLinks = document.querySelector(".navlinks")
 
-menuBtn.addEventListener("click",()=>{
+menuBtn.addEventListener("click", () => {
     navLinks.classList.add("show")
     cancel.classList.add("cancel-icon-show")
 })
-cancel.addEventListener("click",()=>{
+cancel.addEventListener("click", () => {
     navLinks.classList.remove("show")
     cancel.classList.remove("cancel-icon-show")
 })
+
+// Add Scroll Animation
+
+let sections = document.querySelectorAll(".section")
+
+window.onscroll = () => {
+    sections.forEach((sec) => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 550;
+        let height = sec.offsetHeight;
+       if(top >= offset && top < offset + height){
+        sec.classList.add("show-animate")
+       }else{
+        if(top < offset + height - 200){
+            sec.classList.remove("show-animate")
+        }
+       }
+    })
+}
